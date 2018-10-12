1bd64b9eb8de83123d46f44577be0ddb1f87c777
z5163204 <z5163204@cse.unsw.edu.au>
2018-10-12 22:13:56 +1100

    opened a file wow

--- Part_1/readData.c
+++ Part_1/readData.c
@@ -1,20 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "readData.h"
 
-List listOfUrls(char* file){
+void listOfUrls(char file[]){
 	//Find the file
+    char * filename = malloc(strlen(file) + strlen(".txt") + 1);
+    strcpy(filename, file);
+    strcat(filename,".txt");
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "error: file collection.txt can not open\n");
+        return;
+    }
+
+    char *str;
+
+
+    while(fscanf(fp, "%s", str) != EOF) {
+        printf("%s\n", str);
+    }
+
+    fclose(fp);
+    free(filename);
+
 	//Make a list (Queue/Stack) of the URLS
-	//Return the URL list 
+	//Return the URL list
 }
-Graph getGraph(List urls) {
+
+int main() {
+
+    listOfUrls("collection");
+    return 0;
+
+}
+
+//Graph getGraph(List urls) {
 	//Create an empty list
 	//Get first element of the list
 	//While list is not empty
 		//Read the url
 		//Update graph by adding node and outgoinf links
 	//End while
-}
-List getInvertedList(List urls) {
+//}
+//List getInvertedList(List urls) {
 	//Don't understand what this one is suppsed to do
-}
+//}
