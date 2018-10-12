81664fde4ef06a5abd1f0786d5fc044c2547eb59
Tomas <togtja@gmail.com>
2018-10-12 20:29:10 +1100

    Started on psudo code for readData.c

---
+++ Part_1/readData.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "readData.h"
+
+void listOfUrls(char* file){
+	//Find the file
+	//Make a list (Queue/Stack) of the URLS
+	//Return the URL list 
+}
+Graph getGraph(/*A list of URLS*/) {
+	//Create an empty list
+	//Get first element of the list
+	//While list is not empty
+		//Read the url
+		//Update graph with said data
+}
