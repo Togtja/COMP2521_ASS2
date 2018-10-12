4d9acb8f669dcd1407cefb65e74498166df36cbd
togtja <togtja@gmail.com>
2018-10-12 20:55:54 +1100

    More psudo code for readData.c

--- Part_1/readData.c
+++ Part_1/readData.c
@@ -2,15 +2,19 @@
 #include <stdio.h>
 #include "readData.h"
 
-void listOfUrls(char* file){
+List listOfUrls(char* file){
 	//Find the file
 	//Make a list (Queue/Stack) of the URLS
 	//Return the URL list 
 }
-Graph getGraph(/*A list of URLS*/) {
+Graph getGraph(List urls) {
 	//Create an empty list
 	//Get first element of the list
 	//While list is not empty
 		//Read the url
-		//Update graph with said data
+		//Update graph by adding node and outgoinf links
+	//End while
+}
+List getInvertedList(List urls) {
+	//Don't understand what this one is suppsed to do
 }
