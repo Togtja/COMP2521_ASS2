23a863d92cdad7a95204415e49b18cb7e6b9d834
Tomas Berger <tomashb@stud.ntnu.no>
2018-10-23 20:03:22 +1100

    Wrote some BSTree in main

--- Part_1/pagerank.c
+++ Part_1/pagerank.c
@@ -6,7 +6,8 @@
 #include "graph.h"
 #include "pagerank.h"
 #include <math.h>
-
+#include "BSTree.h"
+#include "invertedIndex.h"
 
 void pageRankCalc(List l, double damp, double diffPR, int it);
 float W_in(List l, struct node * src, struct node *p1);
@@ -56,7 +57,9 @@ int main(int argc, char *argv[]) {
 	removeNonLetters(ncnst);
 	printf("%s", ncnst);
 
-	//free(test2);
+	printf("\nBSTree:\n\n");
+	BSTree BST = newBSTree();
+	invIndexBuilder(sort, BST);
 
 	char c = getchar();
 	putchar(c);
