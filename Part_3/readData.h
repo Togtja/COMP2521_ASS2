ee364268447ab89006e6e829a9508bb2366c6000
Tomas Berger <tomashb@stud.ntnu.no>
2018-10-25 15:21:41 +1100

    Part 3 should be done (only bug fixing left)

---
+++ Part_3/readData.h
@@ -0,0 +1,45 @@
+#ifndef READDATA_H
+#define READDATA_H
+
+#include "posix.h"
+
+struct node {
+    char * url;
+    struct node *next;
+    int pos;
+	float TfIdfValue;
+	int rankVal;
+};
+
+struct List {
+
+    struct node *head;
+    struct node *curr;
+    int size;
+
+};
+
+typedef struct List *List;
+
+
+List newList();
+
+void printList(List L);
+
+void printOnlyUrls(List L);
+void fprintOnlyUrls(List L, FILE *fp);
+
+void insertList(char str[], List L);
+
+void listOfUrls(char file[], List l);
+//Copies lists
+List copy(List l);
+//deltes list
+void deleteList(List l);;
+//Bubble sort the Rank Values
+void BubbleSortListRV(List ret);
+//Bubble sort the TFIDF values
+void BubbleSortListTFIDF(List ret);
+//merges two list and frees the second agrument/list
+List mergeList(List l1, List l2);
+#endif
